b7fc50a05c42f5921246cf8d67e5cb2f36fd0701
Shushank25 <saitshushank@gmail.com>
2023-09-24 13:59:54 +0530

    updated code

--- weatherreport-script/WeatherReport.py
+++ weatherreport-script/WeatherReport.py
@@ -1,6 +1,5 @@
 import requests
 
-# OpenWeatherMap API URL
 apiKey = "https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22"
 
 def get_weather_data():
